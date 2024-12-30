5bb52fb6d6a6525f15ac80412948e87e0b9dccc0
Alberto <albertogilsanjuan@gmail.com>
2024-12-30 16:30:22 +0100

    ADD: Router Install & Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from './components/header/Header'
 import { Banner } from './components/banner/Banner'
 import { DashBoard } from './components/dashboard/Dashboard'
